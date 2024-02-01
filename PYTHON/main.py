3f1cfc8318dee59cea8c8b266c30cf89502660ff
gabricampaa <135340970+gabricampaa@users.noreply.github.com>
2024-02-01 16:31:17 +0100

    Create main.py

---
+++ PYTHON/main.py
@@ -0,0 +1,15 @@
+class foo(object):
+    """docstring for foo."""
+
+    def __init__(self, arg):
+        super(foo, self).__init__()
+        self.arg = arg
+
+
+
+
+def main():
+    print("Hello, World!")
+
+if __name__ == '__main__':
+    main()
